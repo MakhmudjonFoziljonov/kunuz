2c8d37ef0fb055803430a50ba631a5ce82c4f953
makhmudfoziljonov <mahmudjonfoziljonov1@gmail.com>
2024-10-18 06:52:48 +0500

    first commit

---
+++ src/test/java/com/kunuz/KunuzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunuzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
